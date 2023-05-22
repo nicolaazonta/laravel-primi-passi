a43d9384dfe0d4dd2e46b5a28b5fc72caabfb2d9
nicolaazonta <nicolazonta114@gmail.com>
2023-05-22 17:31:24 +0200

    create two pages

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>home</title>
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+                background-color: lightseagreen
+            }
+        </style>
+    </head>
+    <body>
+
+        <header>
+            
+        </header>
+        
+    </body>
+</html>
